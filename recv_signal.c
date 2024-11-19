494858d1fee98e390572aa865f0c03bdbfbcefa1
osbornej23 <osbornej@msoe.edu>
2024-11-19 16:49:39 -0600

    send_signal.c and recv_signal.c is finished/commented

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,34 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c
+ * Modified by: Jadyn Osborne
  * 
- * Brief summary of program:
+ * Brief summary of program: Uses SIGUSR1 handler to receieve and print
+ * information from the othe process that is using sigqueue.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handle_sigusr1(int sig, siginfo_t *info, void *context) {
+    printf("Received SIGUSR1 with data: %d, from PID: %d\n",
+           (*info).si_value.sival_int, (*info).si_pid);
+}
+
+int main() {
+
+    pid_t pid = getpid(); // getting this programs pid
+    // printing pid so user can use it in send_signal command line argument
+    printf("The current process ID is: %d\n", pid); 
+
+    struct sigaction sa;
+    sa.sa_sigaction = handle_sigusr1;  // register handler in sa_sigaction field
+    sa.sa_flags = SA_SIGINFO; // enable additional signal info
+
+    sigaction(SIGUSR1, &sa, NULL);    // registering handler for SIGUSR1
+
+    printf("Waiting for SIGUSR1 with data...\n");
+    pause();
+    return 0;
+}
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,44 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Jadyn Osborne
  * 
- * Brief summary of program:
+ * Brief summary of program: Uses the sigqueue function to send a SIGUSR1
+ * signal to another waiting process (recv_signal.c) along with custom data
+ * (a randomly generated integer value)
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+
+    // saw some functions use a similar technique
+    // typed so that if the runner runs with no arguments they know what
+    // arguments are to be expected since nothing happens
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <PID>\n", argv[0]);
+        return 1;
+    }
+    
+    // getting the target pid from the command line
+    pid_t target_pid = atoi(argv[1]);  
+
+    srand(time(NULL));
+    // random integer generation
+    int random_value = rand() % 100;
+
+    union sigval sig_data;
+    sig_data.sival_int = random_value;
+
+    printf("Sending SIGUSR1 with data: %d to PID: %d\n", random_value, target_pid);
+    if (sigqueue(target_pid, SIGUSR1, sig_data) == -1) {
+        perror("sigqueue failed");
+        return 1;
+    }
+
+    return 0;
+}
+
