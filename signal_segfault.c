7a558e06c7861f2a1cabbdef344f884e43513df6
osbornej23 <osbornej@msoe.edu>
2024-11-19 15:15:37 -0600

    signal_segfault.c is finished/commented

--- signal_segfault.c
+++ signal_segfault.c
@@ -4,15 +4,31 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Jadyn Osborne
  * 
- * Brief summary of modifications:
+ * Brief summary of modifications: Added a signal handler for SIGSEGV
  */
 
 
 #include <stdio.h>
+#include <signal.h>
+
+/**
+ * @brief Signal handler for SIGSEGV - prints a message and exits
+ */
+void handle_signal() {
+    printf("Received a segmentation fault violation signal\n");
+    // This does not gracefully terminate the program, because of this, the
+    // program will continuously go back and rerun the line of code that
+    // caused the seg fault signal and repeatedly call this handler.
+    // In this case the sigfault causing code is the dereferencing of a null
+    // pointer in main.
+}
 
 int main (int argc, char* argv[]) {
+    // Register for the signal
+    signal(SIGSEGV, handle_signal);
+
     // Declare a null pointer
     int* i = NULL;
 
