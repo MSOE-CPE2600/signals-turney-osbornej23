87bd295a6d5d5a17cced96c301de357de6074ce6
osbornej23 <osbornej@msoe.edu>
2024-11-19 15:36:26 -0600

    signal_alarm.c is finished/commented

--- signal_alarm.c
+++ signal_alarm.c
@@ -1,6 +1,31 @@
 /**
- * File: 
- * Modified by:
+ * File: signal_alarm.c
+ * Modified by: Jadyn Osborne
  * 
- * Brief summary of program:
+ * Brief summary of program: Triggers an alarm function and sends the resulting
+ * SIGALRM after five seconds to a handler which prints a statement
  */
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+/**
+ * @brief Signal handler for SIGSEGV - prints a message and exits
+ */
+void handle_signal() {
+    printf("Received a signal alarm (SIGALRM)\n");
+}
+
+int main (int argc, char* argv[]) {
+    // Register for the signal
+    signal(SIGALRM, handle_signal);
+
+    // trigger an alarm signal after five seconds
+    alarm(5);
+
+    // wait until the signal alarm is received to exit the program
+    pause();
+    
+    return 0;
+}
